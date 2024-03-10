79d5fe1b3c3e781783250aa91603939635e525bf
Phung Ly <98497119+pkly1219@users.noreply.github.com>
2024-03-09 23:31:30 -0800

    Update employeeTable.c

--- employeeTable.c
+++ employeeTable.c
@@ -4,6 +4,7 @@
 Employee EmployeeTable[] = 
 { 
 {1001, "Daphne Borromeo", "909-555-2134", 8.78}, 
+
 {1011, "Tammy Franklin","213-555-1212", 4.50}, 
 {1140, "Dorris Perl", "310-555-1215", 7.80}, 
 {4011, "Tony Bobcat", "909-555-1235", 6.34},
